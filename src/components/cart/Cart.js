a463a1be806a8b221d91e6237383a20106a02380
Nusrat Jahan <njnusrat53@gmail.com>
2020-08-26 17:17:30 +0600

    cart completed

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import './Cart.css';
 
 function Cart ({ cart }) {
-	console.log(cart);
 	const totalPrice = cart.reduce((acc, curr) => {
 		return acc + curr.price;
 	}, 0);
+
+	const shippingCost = cart.reduce((acc, curr) => {
+		return acc + curr.shipping;
+	}, 0);
+
+	const tax = totalPrice / 10;
+
 	return (
-		<div>
-			<h4>Order Summery</h4>
+		<div className="cart-content">
+			<h3>Order Summery</h3>
 			<p>Items Ordered: {cart.length} </p>
-			<h3>Price:{totalPrice.toFixed(2)}</h3>
+			<p>Product price: ${totalPrice.toFixed(2)}</p>
+			<p>
+				<small>Shipping cost:${shippingCost.toFixed(2)}</small>
+			</p>
+			<p>
+				<small>Tax + VAT: ${tax.toFixed(2)}</small>
+			</p>
+			<hr />
+			<h4>Total Price:${(totalPrice + shippingCost + tax).toFixed(2)}</h4>
 		</div>
 	);
 }
